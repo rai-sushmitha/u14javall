573c2f5cda7995588f8ad3fd58d4853d7eeec528
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-14 17:51:43 +0530

    changes

--- version/openjdk9.sh
+++ version/openjdk9.sh
@@ -4,11 +4,11 @@ echo "================ Installing openjdk-9-jdk ================="
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install -y openjdk-9-jdk
-sudo update-alternatives --set java /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-9-openjdk-amd64/bin/javac
-sudo add-apt-repository ppa:maarten-fonville/ppa
-sudo apt-get update
-sudo apt-get install icedtea-9-plugin
-sudo update-alternatives --set javaws /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/javaws
+#sudo update-alternatives --set java /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/java
+#sudo update-alternatives --set javac /usr/lib/jvm/java-9-openjdk-amd64/bin/javac
+#sudo add-apt-repository ppa:maarten-fonville/ppa
+#sudo apt-get update
+#sudo apt-get install icedtea-9-plugin
+#sudo update-alternatives --set javaws /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/javaws
 echo 'export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/lib/jvm/java-9-openjdk-amd64/jre/bin' >> $HOME/.bashrc
